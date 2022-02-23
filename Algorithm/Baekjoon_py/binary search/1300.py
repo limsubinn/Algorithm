4040c28d4cd4bd9e8bee9ab7f7575472aa2404f6
limsubinn <mymyx02@naver.com>
2022-02-23 22:39:10 +0900

    -

---
+++ Algorithm/Baekjoon_py/binary search/1300.py
@@ -0,0 +1,19 @@
+n = int(input())
+k = int(input())
+
+start = 1
+end = k
+
+while start <= end:
+    mid = (start + end) // 2
+    cnt = 0
+
+    for i in range(1, n+1):
+        cnt += min(mid//i, n)
+
+    if cnt >= k:
+        end = mid - 1
+    else:
+        start = mid + 1
+
+print(start)
