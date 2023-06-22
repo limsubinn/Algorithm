e8b2fecb7c23c5d6fc6b0a75d48040b3d5a806f5
limsubinn <mymyx02@naver.com>
2023-06-22 11:36:04 +0900

    add boj2167(2차원 배열의 합)

---
+++ Baekjoon/dynamic programming/2167.py
@@ -0,0 +1,19 @@
+import sys
+
+def input():
+    return sys.stdin.readline().strip()
+
+n, m = map(int, input().split())
+data = [list(map(int, input().split())) for _ in range(n)]
+
+# 누적합
+dp = [[0] * (m+1) for _ in range(n+1)]
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        dp[i][j] = data[i-1][j-1] + dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1]
+
+k = int(input())
+for _ in range(k):
+    i, j, x, y = map(int, input().split())
+    answer = dp[x][y] - dp[x][j-1] - dp[i-1][y] + dp[i-1][j-1]
+    print(answer)
